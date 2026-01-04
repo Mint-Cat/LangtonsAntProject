c809d1e635d7c947a2b91176e365d4b982f12de1
Kat <katja.schloissnig@stud.hcw.ac.at>
2026-01-04 17:03:37 +0100

    ScreenSize

--- src/main/java/at/ac/hcw/langtonsantproject/HelloApplication.java
+++ src/main/java/at/ac/hcw/langtonsantproject/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start-screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Langton's Ant Simulation");
         stage.setScene(scene);
         stage.show();
