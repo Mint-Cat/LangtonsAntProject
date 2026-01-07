8366fe4e11712873fb1dffe41af45e48e210124c
Kat <katja.schloissnig@stud.hcw.ac.at>
2026-01-07 13:41:48 +0100

    Added UI-Elements in Start & Setting

--- src/main/java/at/ac/hcw/langtonsantproject/App.java
+++ src/main/java/at/ac/hcw/langtonsantproject/App.java
@@ -11,6 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("start-screen.fxml"));
+        fxmlLoader.getController();
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Langton's Ant Simulation");
         stage.setScene(scene);
--- src/main/java/at/ac/hcw/langtonsantproject/StartScreenController.java
+++ src/main/java/at/ac/hcw/langtonsantproject/StartScreenController.java
@@ -1,10 +1,33 @@
 package at.ac.hcw.langtonsantproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class StartScreenController {
    @FXML
-    public Label startscreen;
+   public Label startscreen;
+   public VBox VBox1;
+   public VBox VBox2;
+   public Button NewAntButton;
+   public Button LoadAntButton;
+   public Button DeleteAntButton;
+ public Label NameLabel;
 
+
+ @FXML
+ private void initialize() {
+
+  NameLabel.setStyle("-fx-font-size: 60" );
+
+ }
+
+ public void NewAntButtonClick(ActionEvent actionEvent) {
+   }
+   public void LoadAntButtonClick(ActionEvent actionEvent) {
+   }
+   public void DeleteAntButtonClick(ActionEvent actionEvent) {
+   }
 }
