4cbdf20b045d9cb1a3092bd24e87a21a69d72b9f
Lika <lika.kevlishvili@hcw.ac.at>
2026-01-19 20:24:32 +0100

    Die Kommentare 1.

--- src/main/java/at/ac/hcw/langtonsantproject/Persistence/SimulationState.java
+++ src/main/java/at/ac/hcw/langtonsantproject/Persistence/SimulationState.java
@@ -1,6 +1,9 @@
 package at.ac.hcw.langtonsantproject.Persistence;
 import at.ac.hcw.langtonsantproject.Misc.AntOrientation;
-
+/**
+ * Captures the current state of a running simulation for persistence.
+ * This class allows users to resume the ant's progress exactly where they left off.
+ */
 public class SimulationState {
     public SettingsState settings;
 
